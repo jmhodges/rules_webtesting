b49b775afe1b61560e47f6d39e938f1805af5f01
Jeff Hodges <jeff@somethingsimilar.com>
2017-07-05 13:28:21 -0700

    localhost hack for macOS 10.12.5
    See https://github.com/bazelbuild/rules_webtesting/issues/150 and
    https://github.com/golang/go/issues/20904

--- go/httphelper/http_helper.go
+++ go/httphelper/http_helper.go
@@ -21,11 +21,8 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
-	"net"
 	"net/http"
 	"net/url"
-	"os"
-	"sort"
 	"strings"
 )
 
@@ -136,29 +133,5 @@ func (s longestToShortest) Less(i, j int) bool {
 // FQDN returns the fully-qualified domain name (or localhost if lookup
 // according to the hostname fails).
 func FQDN() (string, error) {
-	hostname, err := os.Hostname()
-	if err != nil {
-		// Fail if the kernel fails to report a hostname.
-		return "", err
-	}
-
-	addrs, err := net.LookupHost(hostname)
-	if err != nil {
-		return "localhost", nil
-	}
-
-	for _, addr := range addrs {
-		if names, err := net.LookupAddr(addr); err == nil && len(names) > 0 {
-			sort.Sort(longestToShortest(names))
-			for _, name := range names {
-				name = strings.TrimRight(name, ".")
-				if strings.HasPrefix(name, hostname) {
-					return name, nil
-				}
-			}
-			return names[0], nil
-		}
-	}
-
 	return "localhost", nil
 }
